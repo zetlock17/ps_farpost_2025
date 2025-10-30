34b77c40e5c0ab3a9ba9141419ce6d91c01a6e6e
pukita <pukita@MacBook-Pro-pukita.local>
2025-10-30 21:34:28 +1000

    try help front docker

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path' 
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   envDir: '../../',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
+  },
+  resolve: {
+    alias: {
+      "ui-kit": path.resolve(__dirname, "../../packages/ui-kit/src") 
+    }
+  }
 })
